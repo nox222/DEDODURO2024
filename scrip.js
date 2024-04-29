8e2b0b59d10dc0e7f8f7fefafc135c8c25e1e857
rNox1 <128769095+rNox1@users.noreply.github.com>
2024-04-29 08:00:31 -0300

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,10 @@
+
+
+// Exibe uma mensagem ao clicar no botão de download
+document.getElementById('downloadBtn').addEventListener('click', function () {
+    alert('Você está sendo redirecionado para uma nova página');
+    window.location.href = 'https://bibliotecaelfica.org/category/dd-5e/';
+});
+document.getElementById('Desbravando').addEventListener('click', function () {
+    window.location.href = 'fds.html';
+});
