8b753f614e10d6b7868bd731a3bb3f669b0eb500
rNox1 <128769095+rNox1@users.noreply.github.com>
2024-04-29 07:42:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// Rola suavemente até a âncora quando um link é clicado
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
 
 // Exibe uma mensagem ao clicar no botão de download
 document.getElementById('downloadBtn').addEventListener('click', function () {
     alert('Você está sendo redirecionado para uma nova página');
     window.location.href = 'https://bibliotecaelfica.org/category/dd-5e/';
 });
+document.getElementById('Desbravando').addEventListener('click', function () {
+    window.location.href = 'fds.html';
+});
+
