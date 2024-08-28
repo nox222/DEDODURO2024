71d8eefd4ba6a5930bdaa04e8bfca49515b676c5
rNox1 <128769095+rNox1@users.noreply.github.com>
2024-08-28 09:42:29 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,10 +1,1 @@
 
-
-// Exibe uma mensagem ao clicar no botão de download
-document.getElementById('downloadBtn').addEventListener('click', function () {
-    alert('Você está sendo redirecionado para uma nova página');
-    window.location.href = 'https://bibliotecaelfica.org/category/dd-5e/';
-});
-document.getElementById('Desbravando').addEventListener('click', function () {
-    window.location.href = 'index.html';
-});
